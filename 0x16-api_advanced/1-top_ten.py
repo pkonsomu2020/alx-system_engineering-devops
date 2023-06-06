09cc1b0e8df44aeafa86d3dd7297b831119d7019
pkonsomu2020 <pkonsomu2020@gmail.com>
2023-06-06 09:56:52 -0700

    Final push

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,9 +1,17 @@
+#!/usr/bin/python3
+"""
+Queries the `Reddit API`, prints the titles of the first 10
+hot posts listed for a given subreddit
+"""
+
 import requests
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = "https://www.reddit.com/r/{subreddit}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}  # Set a custom User-Agent header
     response = requests.get(url, headers=headers)
+
+    data = requests.get(url, headers=header, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
